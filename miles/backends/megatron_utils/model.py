1c7019f31132b1f150fda09b018b58ee663a8f22
Mathew Han <mathewjhan@gmail.com>
2026-05-04 00:17:08 -0700

    [fix] naming

--- miles/backends/megatron_utils/model.py
+++ miles/backends/megatron_utils/model.py
@@ -255,9 +255,9 @@ def forward_only(
         response_lengths = batch["response_lengths"]
 
         if "adapter_token_counts" in batch:
-            from megatron.bridge.peft.multi_lora_layers import set_batch
+            from megatron.bridge.peft.multi_lora_layers import set_tokens_per_adapter_slot
 
-            set_batch(model, batch["adapter_token_counts"])
+            set_tokens_per_adapter_slot(model, batch["adapter_token_counts"])
 
         output_tensor = model(
             input_ids=tokens,
@@ -406,9 +406,9 @@ def train_one_step(
         )
 
         if "adapter_token_counts" in batch:
-            from megatron.bridge.peft.multi_lora_layers import set_batch
+            from megatron.bridge.peft.multi_lora_layers import set_tokens_per_adapter_slot
 
-            set_batch(model, batch["adapter_token_counts"])
+            set_tokens_per_adapter_slot(model, batch["adapter_token_counts"])
 
         from miles.utils.replay_base import all_replay_managers
 
